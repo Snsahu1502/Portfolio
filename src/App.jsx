24121db17d7d22bc713556ccd46454e8603ff975
Shivnarayan Sahu <97087216+Snsahu1502@users.noreply.github.com>
2025-05-29 14:06:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./Components/About/About"
 import Experience from "./Components/Experience/Experience"
-import Footer from "./Components/Footer/footer"
+import Footer from "./Components/Footer/Footer"
 import Home from "./Components/Home/Home"
 import Navbar from "./Components/Navbar/Navbar"
 import Project from "./Components/Project/Project"
